4af2d335ea286f41cc24294074e73c791868599a
Navya <navyaburrewar2721@gmail.com>
2026-02-09 22:50:49 +0530

    checking palindrome

---
+++ day 60/challenge-9.py
@@ -0,0 +1,8 @@
+## Write a function that checks if a string is a palindrome.
+
+def palindrome(name):
+    if name==name[: : -1]:
+        print("palindrome")
+    else:
+        print("not a palindrome")
+palindrome("navyayvan")  
