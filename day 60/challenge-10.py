77552b4aed78e6a0cfc364d994bf8c852347f940
Navya <navyaburrewar2721@gmail.com>
2026-02-09 22:59:51 +0530

    fibonacci

---
+++ day 60/challenge-10.py
@@ -0,0 +1,10 @@
+##    Write a function that returns the Fibonacci sequence up to n terms.
+
+def fibonacci(n):
+    a=0
+    b=1
+    for i in range(n):
+       print(a,end=" ")
+       a,b=b,a+b
+fibonacci(10)       
+
