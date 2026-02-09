ef3c3cd6e614fd0e53d6826b91b45dece2b20d29
Navya <navyaburrewar2721@gmail.com>
2026-02-09 22:09:45 +0530

    prime

--- day 60/challenge -5.py
+++ day 60/challenge -5.py
@@ -11,13 +11,4 @@ def prime(m):
 print(prime(2))    
     
 
-def prime(n):
-    if n <= 1:
-        return False
-    
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    
-    return True
-print(prime(2))
+
