b42e1a063cb3d45bc4b80853d98c7cbecf4b10ce
Navya <navyaburrewar2721@gmail.com>
2026-02-12 22:55:36 +0530

    choti_1

---
+++ day 60/challenge-14.py
@@ -0,0 +1,12 @@
+
+def counting_vowels(name):
+    count=0
+    vowels = "aeiou"
+    for char in name:
+        if char in vowels:
+           count+=1
+
+    return count
+     
+
+print(counting_vowels("choti"))     
