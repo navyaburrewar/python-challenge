c8f255646fb65861314fa5f60db6ee2c1835fde8
Navya <navyaburrewar2721@gmail.com>
2026-02-13 22:03:43 +0530

    prime number

---
+++ day 60/challenge 16.py
@@ -0,0 +1,12 @@
+## Write a function that checks whether a number is prime.
+
+def prime(m):
+    if m<=1:
+        return False
+    for i in range(2,m):
+        if m%i==0:
+            return False
+        
+    return True
+print(prime(10))    
+    
