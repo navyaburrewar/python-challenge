c4a52174a50e44838552fa4c9daff996336747c8
Navya <navyaburrewar2721@gmail.com>
2026-02-20 19:06:23 +0530

    parameter names

---
+++ day 63 (parameter vs arguments 63 ).py/prob-3.py
@@ -0,0 +1,8 @@
+## Print parameter names and argument values inside the function.
+
+def show(product,price,quality):
+    print("produnct=",product)
+    print("price=", price)
+    print("quality",quality)
+
+show("bag", 1000,"good" )    
