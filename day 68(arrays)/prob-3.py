fc4e78c945d6dd3688793977d6e52c07a39a8e21
Navya <navyaburrewar2721@gmail.com>
2026-02-22 23:57:52 +0530

    numpy

---
+++ day 68(arrays)/prob-3.py
@@ -0,0 +1,17 @@
+from numpy import *
+arr=(3,4,5,6)
+arr1=(6,7,8)
+
+
+print(sum(arr))
+print(max(arr))
+print(min(arr))
+
+
+arr =array([1,3,5,7,9,2])
+arr1=array([3,4,7,9])
+print(sin(arr))
+print(log(arr))
+print(cos(arr))
+print(sqrt(arr))
+print(sqrt(arr))
