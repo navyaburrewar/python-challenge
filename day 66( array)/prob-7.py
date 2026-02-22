858ba8f1f6c442e9413e5f1063c25dff30b04ecb
Navya <navyaburrewar2721@gmail.com>
2026-02-22 23:18:26 +0530

    numpy

--- day 66( array)/prob-7.py
+++ day 66( array)/prob-7.py
@@ -1,12 +1,4 @@
 
-# # from numpy import *
-
-# # arr=array([1,2,3,4,5],float)
-
-# # print(arr.dtype)
-# # print(arr)
-
-
 
 import numpy 
 print(numpy.__version__)
@@ -19,3 +11,10 @@ arr=array([1,2,3,4,5],float)
 
 print(arr.dtype)
 print(arr)
+
+
+## here which convert the one form to anotherform 
+# which will change and accesing multiple inputs b                
+# but converting them into the one type
+
+
