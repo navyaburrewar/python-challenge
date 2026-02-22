386fb911511e6e3cc80698152a52bf49b87906c1
Navya <navyaburrewar2721@gmail.com>
2026-02-22 22:36:45 +0530

    array(float)

---
+++ day 65( array)/prob-3.py
@@ -0,0 +1,25 @@
+## user input of array
+
+from array import *
+
+arr =array('i',[])
+n=int(input("num"))
+for i in range(n):
+    x=int(input())
+    arr.append(x)
+print(arr)    
+
+
+
+#### 2 problems
+
+
+from array import *
+
+arr=array('f',[])
+n=int(input())
+
+for i in range(n):
+    x=int(input())
+    arr.append(x)
+print(arr)      
