96f06c0504d546bbb38c695627679d4c66015528
Navya <navyaburrewar2721@gmail.com>
2026-02-22 22:18:59 +0530

    array-1

---
+++ day 65( array)/theory-1.py
@@ -0,0 +1,11 @@
+
+# Arrays in Python
+# In Python, an array is a data structure used to store multiple values in a single variable. There are different ways to work with arrays in Python depending on your needs.
+
+
+import array
+
+numbers = array.array('i', [1, 2, 3, 4])
+numbers.append(5)
+
+print(numbers)
