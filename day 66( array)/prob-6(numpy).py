b6ce82c1f69ce4144f9a868aafa015d748d09f88
Navya <navyaburrewar2721@gmail.com>
2026-02-22 22:56:18 +0530

    numpy

--- day 66( array)/prob-6(numpy).py
+++ day 66( array)/prob-6(numpy).py
@@ -3,7 +3,14 @@
 ##to work with the multiple dimentional and array and to do some math calucations we need to install numpy and we have to import the numpy  
 #install in command promp 
 
-## 6 base of array   all the below are  the functions from the numpy
+## 6 base of array all the below are  the functions from the numpy
 
 
+
+# array()
+# linespace()
+# logspace()
+# arrange()
+# zeros()
+# ones()
 
