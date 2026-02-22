b3e73c845f7c0af87b5a6b80c933f3d7077643ad
Navya <navyaburrewar2721@gmail.com>
2026-02-22 23:15:26 +0530

    numpy

---
+++ day 66( array)/prob-7.py
@@ -0,0 +1,21 @@
+
+# # from numpy import *
+
+# # arr=array([1,2,3,4,5],float)
+
+# # print(arr.dtype)
+# # print(arr)
+
+
+
+import numpy 
+print(numpy.__version__)
+
+
+
+from numpy import *
+
+arr=array([1,2,3,4,5],float)
+
+print(arr.dtype)
+print(arr)
