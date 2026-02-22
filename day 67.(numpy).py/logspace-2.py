e811a06a6986340767e20d9d938e6809a2711e2c
Navya <navyaburrewar2721@gmail.com>
2026-02-22 23:29:23 +0530

    logspace

---
+++ day 67.(numpy).py/logspace-2.py
@@ -0,0 +1,12 @@
+# 4. logspace()
+
+# the space b/w the that which will depends on the log
+from numpy import*
+arr =logspace(1,20,5)
+print(arr)
+
+
+# if you dont want to print the e then do the follwing
+
+arr =logspace(1,20,5)
+print('%.2f' %arr[4])
