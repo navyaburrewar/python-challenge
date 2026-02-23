73153d878253aa5d3a323dcaac269f0c3da16844
Navya <navyaburrewar2721@gmail.com>
2026-02-23 12:27:49 +0530

    array concepts

---
+++ day 69.py/concepts(array).py
@@ -0,0 +1,103 @@
+# 🔹 1. Creating Arrays (Lists)
+
+# Empty list → arr = []
+# With values → arr = [1, 2, 3]
+# Using list() constructor
+# List comprehension → [x for x in range(5)]
+
+
+
+# 🔹 2. Indexing
+# Positive indexing → arr[0]
+# Negative indexing → arr[-1]
+# Accessing elements by position
+
+
+
+
+# 🔹 3. Slicing
+# arr[start:end]
+# arr[start:end:step]
+# Reverse slicing → arr[::-1]
+
+
+
+
+# 4. Traversing (Iteration)
+# Using for element in arr
+# Using for i in range(len(arr))
+# Using while loop
+# Using enumerate()
+
+
+
+# 🔹 5. Updating Elements
+# arr[index] = value
+
+
+
+
+# 🔹 6. Adding Elements
+
+# append()
+# insert()
+# extend()
+# Concatenation using +
+
+
+
+
+# 7. Removing Elements
+# remove()     # num
+# pop()        # index
+# del           #index
+# clear()       #3 removes all elements
+
+
+
+
+
+# 🔹 8. Built-in List Functions
+
+# len()
+# min()
+# max()
+# sum()
+# sorted()
+
+
+
+
+# 9. List Methods
+
+# sort()
+# reverse()
+# count()
+#  index()
+# copy()
+
+
+
+
+# 10. Membership Operators
+# in
+# not in
+
+
+
+
+# 11. Nested Lists (2D Arrays)
+# Matrix representation
+# Accessing elements → arr[row][col]
+
+
+
+
+# 12. Shallow Copy vs Deep Copy
+# arr.copy()
+# list(arr)
+# copy module (for deep copy)
+
+
+
+
