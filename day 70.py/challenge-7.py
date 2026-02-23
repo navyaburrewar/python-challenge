0c9ccd19a87fdb7ec4fb21e3377665870177a50e
Navya <navyaburrewar2721@gmail.com>
2026-02-23 15:19:42 +0530

    max and min

--- day 70.py/challenge-7.py
+++ day 70.py/challenge-7.py
@@ -7,3 +7,26 @@ arr=array('i',[2,3,6,8,9])
 
 print(max(arr))
 print(min(arr))
+
+
+
+
+## another method
+
+
+from array import array
+
+arr=array('i',[2,3,6,8,9])
+
+max_val=arr[0]
+min_val=arr[0]
+
+for i in arr:
+    if i>max_val:
+        max_val = i
+    if i < min_val:
+        min_val = i
+
+print("max:", max_val)
+print("min :",min_val)        
+
