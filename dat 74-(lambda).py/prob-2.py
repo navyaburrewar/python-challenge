c9ef22fba3d88d9699c6d5216b017264c9991a76
Navya <navyaburrewar2721@gmail.com>
2026-02-28 22:18:01 +0530

    add

--- dat 74-(lambda).py/prob-2.py
+++ dat 74-(lambda).py/prob-2.py
@@ -1,10 +1,9 @@
-## Write a lambda function that returns "Even" if a number is even, otherwise "Odd".
+# ## Write a lambda function that returns "Even" if a number is even, otherwise "Odd".
 
 
-list=[2,3,4,5,6,7,8,9]
+# list=[2,3,4,5,6,7,8,9]
 
-even_odd = lambda num :  "even" if num%2==0 else "odd"
-print(even_odd(4))
-print(even_odd(1))
+# even_odd = lambda num :  "even" if num%2==0 else "odd"
+# print(even_odd(4))
+# print(even_odd(1))
            
-     
