8338c2f967ae14f7d37af63a44f8b906233b752f
Navya <navyaburrewar2721@gmail.com>
2026-03-04 08:43:42 +0530

    printing character

---
+++ day-78(generators).phy/pron-9.py
@@ -0,0 +1,9 @@
+## Create a generator that reads a sentence and yields words one by one.
+
+def sen__1(sentence):
+    for word in sentence.split():
+        yield word
+
+
+for word in sen__1("she is a good girl"):
+    print(word)        
