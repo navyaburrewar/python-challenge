44af6f37a2d44c023dc8af2a4cfa0ef799aa9395
Navya <navyaburrewar2721@gmail.com>
2026-03-08 22:08:04 +0530

    str-1

---
+++ str.py
@@ -0,0 +1,17 @@
+##3 using send()
+
+
+def generator():
+    x=yield "start"
+    print("recived",x)
+
+    y=yield "middle"
+    print("received:",y)
+
+    yield "end"
+
+g=generator()
+
+print(next(g))
+print(g.send(11))
+print(g.send(11))
