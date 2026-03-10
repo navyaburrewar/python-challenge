c45f8b7104a17ab8ea44d48e3dc84cf80631b24b
Navya <navyaburrewar2721@gmail.com>
2026-03-10 18:55:01 +0530

    file4

---
+++ day 81(module)/file4.py
@@ -0,0 +1,9 @@
+## Variables in Module
+# The module can contain functions, as already described, but also variables of all types (arrays, dictionaries, objects etc):
+
+person1 ={
+
+    "name ": "navya",
+    "age": 35,
+    "country": "india"
+}
