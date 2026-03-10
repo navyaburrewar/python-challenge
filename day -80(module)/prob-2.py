fb54f38a2dac925315d0d3b1aabd10642a4cf7db
Navya <navyaburrewar2721@gmail.com>
2026-03-10 08:38:59 +0530

    operations

--- day -80(module)/prob-2.py
+++ day -80(module)/prob-2.py
@@ -14,3 +14,25 @@ def multipliacation( m,n) :
     print(m*n)
 
 multipliacation(4,5)    
+
+
+
+
+
+
+def sub(a,b):
+    print(a-b)
+
+
+n,m = map(int,input("Enter two numbers: " ).split())
+
+sub(n,m)
+
+
+
+
+def divdide(a,b):
+    print(a%b)
+
+
+divdide(20,2)
