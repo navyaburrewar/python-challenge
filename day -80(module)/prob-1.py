f467a17d19cfb6943dacac9ed4582c58a6ee669f
Navya <navyaburrewar2721@gmail.com>
2026-03-11 23:02:14 +0530

    functions

--- day -80(module)/prob-1.py
+++ day -80(module)/prob-1.py
@@ -1,23 +1,29 @@
 # 
-# fibonacci series
+# fibonacci series 
+# n=5
+# 0 ,1, 1,2,3
 
 
 
-def fib(m):
+def fib(m):  # m=5
+  
     if m ==0:
         return 0
     if m==1:
         return 1
-    
+       
     else :
+        
         return fib(m-1)+fib(m-2)
     
 
-
-print(fib(5))
+n=int(input())
+for i in range(n):
+  print(fib(i),end=" ")
 
 
 ### since f(n)==f(n-1)+f(n-2) this  is corect, but not this f(n)=(n-1)+(n-2)
+
 
 
 
---
+++ day 90 (problem)/pro-1.py
@@ -0,0 +1,36 @@
+
+
+## keyword arguments
+
+def fun(fname,lname):
+    print("Hi Im"+" "+fname+" "+lname)
+fun(fname='bhavani',lname='devi')
+
+
+
+## positional  aruguments
+
+def fun1(fn,ln):
+    print("Hlo"+fn+ln)
+fun1('how', 'areyou')
+
+
+
+## positional using args which will take n number of inputs
+
+
+def fun(*names):#*args
+    return "Hi my name is"+" "+ names[2]
+
+print(fun("a","b","c","d"))
+
+
+
+
+## keyword arguments infinate values 
+
+
+def fun1(**names):
+    return "Hi my name is"+" "+names["lname"]
+print(fun1(fname="a",lname="b"))
+    
