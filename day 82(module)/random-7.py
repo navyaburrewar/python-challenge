123ba24806351f69544b26e4ccb810902451243e
Navya <navyaburrewar2721@gmail.com>
2026-03-11 07:52:07 +0530

    shuffle

---
+++ day 82(module)/random-7.py
@@ -0,0 +1,11 @@
+## random shuffle
+
+import random
+list=[2,4,6,8,9]
+random.shuffle(list)
+print(list)
+
+
+## here create new list which will retuen none
+# for the sam list list shuffle and return
+
