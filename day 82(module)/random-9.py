886d0235d8b091e4a79b1bf76d2a5206a1f9c77a
Navya <navyaburrewar2721@gmail.com>
2026-03-11 07:58:59 +0530

    rolling problem

---
+++ day 82(module)/random-9.py
@@ -0,0 +1,21 @@
+## dice rolling problem
+
+## 1
+
+import random
+dice =random.randint(1,6)
+print(dice)
+
+
+
+## 2
+
+
+## by using the list
+import random
+dice=[1,2,3,4,5,6]
+print(random.choice(dice))
+
+
+
+
