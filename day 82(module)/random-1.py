1cebf86098501f4c51484a7803cc7ca828d385bf
Navya <navyaburrewar2721@gmail.com>
2026-03-11 07:33:19 +0530

    theory

---
+++ day 82(module)/random-1.py
@@ -0,0 +1,10 @@
+## 🎲 Random Module in Python
+
+# The random module is a built-in Python module used to generate random numbers and make random selections.
+# You don’t need to install it — it comes with Python.
+
+
+## just we have to import it
+## using "import random"
+# 
+#  
