8932cd68b6c0f3740d020d2b3b9c3c76ec666300
Navya <navyaburrewar2721@gmail.com>
2026-03-11 07:47:32 +0530

    random

---
+++ day 82(module)/random-6.py
@@ -0,0 +1,13 @@
+## random sampling
+
+
+import random
+nums=[2,6,3,7,2]
+
+print(random.sample(nums,4))
+
+
+
+##  in the code the 4 will indicate how many numbers have to print
+## ex -- 1num use 1
+# ex-- 2 nums use 2...
