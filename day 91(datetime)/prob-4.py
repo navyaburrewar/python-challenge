53dfbd54bfa645731db0e4742664d3022a4445bd
Navya <navyaburrewar2721@gmail.com>
2026-03-12 18:45:07 +0530

    date components

---
+++ day 91(datetime)/prob-4.py
@@ -0,0 +1,11 @@
+## access date components
+
+## date components which are nothing but the 
+## year,month,day
+
+from datetime import date
+
+today= date.today()
+print(today.day)
+print(today.month)
+print(today.year)
