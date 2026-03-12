63dbdd2ce58344b438a7e547c899181f39480ee0
Navya <navyaburrewar2721@gmail.com>
2026-03-12 18:50:45 +0530

    only time

---
+++ day 91(datetime)/prob-5.py
@@ -0,0 +1,13 @@
+## time class
+
+## time class represents only time
+## components of time are sec,min,hours
+
+from datetime import time
+
+t=time(20,3,4)
+print(t)
+print("seconds",t.second)
+print("min",t.minute)
+print("hour",t.hour)
+
