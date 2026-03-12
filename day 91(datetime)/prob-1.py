07713a00ae403b51d95fc6e7ae075f91c36258b2
Navya <navyaburrewar2721@gmail.com>
2026-03-12 18:37:09 +0530

    present datetime

---
+++ day 91(datetime)/prob-1.py
@@ -0,0 +1,18 @@
+## imporing  the module
+
+### we can importin two ways
+
+import datetime
+
+##or
+
+import datetime as datetime
+
+
+####  prob-1
+## fincding present date and time
+
+import datetime
+
+print(datetime.datetime.now())
+
