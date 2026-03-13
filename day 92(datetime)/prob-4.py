2acb99872ff333715200f10ad323ead9e10e7bcc
Navya <navyaburrewar2721@gmail.com>
2026-03-13 23:53:31 +0530

    timestamp-datetime

---
+++ day 92(datetime)/prob-4.py
@@ -0,0 +1,15 @@
+## 13. Convert Timestamp to Date 
+
+
+from datetime import datetime 
+ts = 1710236000 
+date = datetime.fromtimestamp(ts)
+print(date)
+
+
+## prob-2
+
+from datetime import datetime
+ts=1712236049
+date_1= datetime.fromtimestamp(ts)
+print(date_1)
