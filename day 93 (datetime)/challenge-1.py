78fda815548ed7186d0239da7976b3dfd788d9e2
Navya <navyaburrewar2721@gmail.com>
2026-03-14 23:44:59 +0530

    current date and times

---
+++ day 93 (datetime)/challenge-1.py
@@ -0,0 +1,29 @@
+#1️⃣ Current Date & Time
+
+# Write a program to print:
+# Current date
+# Current time
+# Current date & time together
+
+
+##### Current date
+from datetime import date
+
+current_date =date.today()
+print(current_date)
+
+
+
+# Current time
+from datetime import datetime
+current_time_ = datetime.now().time()
+print(current_time_)
+
+
+
+## date and time
+
+from datetime import datetime
+
+date_time =datetime.now()
+print(date_time)
