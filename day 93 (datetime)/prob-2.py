e811a29f3e5363675c6155d01e82e5ddfd811333
Navya <navyaburrewar2721@gmail.com>
2026-03-14 22:00:56 +0530

    remaing dates

--- day 93 (datetime)/prob-2.py
+++ day 93 (datetime)/prob-2.py
@@ -7,4 +7,15 @@ from datetime import datetime
 future =datetime(2026,12,31)
 today =datetime(2026,3,14)
 remaining_days= future-today
-print(remaining_days)
+print(remaining_days.days)
+
+
+
+
+####   the below one gives the  complete date and time
+
+from datetime import datetime
+future =datetime(2026,12,31)
+today =datetime.now()
+remaining= future-today
+print(remaining)
