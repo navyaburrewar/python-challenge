fbf40153197892bfbe28b71bd967166ef5aeaa5c
Navya <navyaburrewar2721@gmail.com>
2026-03-19 21:48:57 +0530

    streak-2

---
+++ day -80(module)/streak -4.py
@@ -0,0 +1,9 @@
+## Write a recursive function to find the greatest common divisor (GCD) of two numbers.
+
+
+def gcd(m,n):
+    if n==0:
+        return m
+    else :
+        return gcd(n, m%n)
+print(gcd(20,3))    
