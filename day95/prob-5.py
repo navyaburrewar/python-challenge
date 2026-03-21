70aa36e592819a68f4a5f05dd0e9c2f60401afc1
Navya <navyaburrewar2721@gmail.com>
2026-03-21 20:59:59 +0530

    command line one by one

--- day95/prob-5.py
+++ day95/prob-5.py
@@ -3,7 +3,29 @@
 # Write a program that prints all command-line arguments one by one.
 
 
-import sys
-print("scripi line", sys.argv[0])
-print("scripi line", sys.argv[1])
-print("scripi line" ,sys.argv[2])
+# import sys
+# print("scripi line", sys.argv[0])
+# print("first argument", sys.argv[1])
+# print("second argument" ,sys.argv[2])
+
+
+### the about  code is not fully correct
+
+
+
+# import sys
+# i =0
+# for arg in sys.argv:
+#     print(f"argument {i}:{arg}")
+#     i+=1
+
+
+
+## another way
+
+# import sys
+# i =0
+# for i in range(len(sys.argv)):
+#     print(f"argument {i}:{sys.argv[i]}")
+    
+
