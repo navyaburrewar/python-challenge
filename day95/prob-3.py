f1cf331028d73063b8510e856263df40ddcbf826
Navya <navyaburrewar2721@gmail.com>
2026-03-21 18:07:12 +0530

    accesing individual arguments

---
+++ day95/prob-3.py
@@ -0,0 +1,10 @@
+## 💻 Example 2 — Accessing Individual Arguments
+
+
+import sys
+
+
+print("script name", sys.argv[0])
+print("first argument", sys.argv[1])
+print("second argument", sys.argv[2])
+
