52db3d55b21c0ba4410b67b276da4c26697c3379
Navya <navyaburrewar2721@gmail.com>
2026-03-21 20:32:54 +0530

    printing one by one

---
+++ day95/prob-5.py
@@ -0,0 +1,9 @@
+# # 1️⃣
+
+# Write a program that prints all command-line arguments one by one.
+
+
+import sys
+print("scripi line", sys.argv[0])
+print("scripi line", sys.argv[1])
+print("scripi line" ,sys.argv[2])
