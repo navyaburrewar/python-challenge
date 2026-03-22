0e0dec806a320541efb4012647e944d09e9514af
Navya <navyaburrewar2721@gmail.com>
2026-03-22 12:05:39 +0530

    prob-10

---
+++ day95/prob-10.py
@@ -0,0 +1,22 @@
+## 6️⃣
+
+# # Write a program that takes three numbers from command line and prints the largest number.
+
+
+import sys
+sys.argv
+max=int(sys.argv[1])
+
+
+if int(sys.argv[2])>max:
+    max = int(sys.argv[2])
+
+if int(sys.argv[3]) >max:
+    max =int(sys.argv[3])
+
+print(max)
+
+             
+    
+      
+
