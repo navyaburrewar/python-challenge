0c4e031dab5bc7cbfdb11a9893f28ec1194fff6d
Navya <navyaburrewar2721@gmail.com>
2026-03-23 22:30:53 +0530

    loop stops

---
+++ day96/prob-3.py
@@ -0,0 +1,14 @@
+# 2️⃣7️⃣ Write code where:
+# a loop stops normally
+# but program continues
+
+
+n=int(input())
+
+for i in range(1, n):
+        if i==5:
+            break
+        print(i) 
+
+print("exit")            
+
