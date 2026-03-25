7c2b85eae4536665d61a7ffafe20bc3bd8412e7b
Navya <navyaburrewar2721@gmail.com>
2026-03-25 22:52:55 +0530

    sys.exit()

---
+++ day97/pprob-1.py
@@ -0,0 +1,48 @@
+# ## sys.exit()
+
+# Stops your Python program immediately.
+
+# Think: “Close the program now.”
+
+
+# import sys
+# print("started")
+
+# sys.exit()
+# print("ended")
+
+
+
+###################333 prob-2
+# Problem 1
+# Print "Welcome" and then stop the program.
+
+
+
+# import sys
+# print("welcome")
+# sys.exit()
+
+# print("end")
+
+
+
+########################### Problem 3
+# Stop the program if a number is negative.
+
+
+# import sys
+# n= int(input())
+
+# if n <0:
+#     print("negative")
+#     sys.exit()
+
+# else:
+#     print(n)
+
+
+
+#####################33 problem 4 #########3
+import sys
+sys.exit("program ended safely")
