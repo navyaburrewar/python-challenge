b7a2cf524a4e9a196212e4554b92d9aab547e952
Navya <navyaburrewar2721@gmail.com>
2026-03-26 23:05:23 +0530

    platform

---
+++ day99(sys.path)/prob-2.py
@@ -0,0 +1,24 @@
+## sys.platform 
+
+
+# #  Tells which operating system you are using
+# import sys
+# print(sys.platform)
+
+
+
+
+# import sys
+
+# if sys.platform == "win32":
+#     print("Windows")
+
+
+import sys
+print("Running on:", sys.platform)
+
+
+
+
+import sys
+print(type(sys.platform))
