ab9f0ef32cafa17bb69dede214a2448d56abbdc3
Navya <navyaburrewar2721@gmail.com>
2026-03-28 00:07:12 +0530

    strip

---
+++ day100/prob-9.py
@@ -0,0 +1,10 @@
+## strip()
+
+## strip() which removes the extra space
+
+import sys
+name=sys.stdin.readline().strip()
+print("hell0",name )
+
+## here when we have values with teh name value 
+#  it will removes the extra values
