0afdafe77060070b1274b3e2672d132db50c76dc
Navya <navyaburrewar2721@gmail.com>
2026-03-27 23:27:24 +0530

    stdin

---
+++ day99(sys.path)/prob-6.py
@@ -0,0 +1,19 @@
+# import sys
+# data =sys.stdin.read()
+# print(data)
+
+
+
+
+
+
+
+import sys
+
+
+data = sys.stdin.readline()
+print(data)
+
+
+
+## about two code are same but the below on takes only one user input
