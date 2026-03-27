976cbe61a305ec30be27ffe4edd907e624d05095
Navya <navyaburrewar2721@gmail.com>
2026-03-28 00:19:19 +0530

    len

---
+++ day100/challenge-1.py
@@ -0,0 +1,10 @@
+# 🟢 Q1. Clean Input
+
+# 📥 Input:
+#    choti   
+# 👉 Print the name without extra spaces
+
+
+import sys 
+name= sys.stdin.readline().strip()
+print(name)
---
+++ day100/challenge-2.py
@@ -0,0 +1,13 @@
+# 🟢 Q2. Length After Cleaning
+
+# 📥 Input:
+#    python   
+# 👉 Print length of string after removing spaces
+
+
+
+import sys
+
+name =sys.stdin.readline().strip()
+
+print(len(name))
