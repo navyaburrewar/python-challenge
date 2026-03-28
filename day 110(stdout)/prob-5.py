4393085c4447cfc25eb59fef2a43dc4ddf2fc43c
Navya <navyaburrewar2721@gmail.com>
2026-03-29 00:08:05 +0530

    single line numbers

---
+++ day 110(stdout)/prob-5.py
@@ -0,0 +1,14 @@
+# 2. Print Numbers in One Line
+
+# Task:
+# Print numbers from 1 to 10 in a single line separated by spaces.
+
+
+
+import sys
+for i in range(11):
+    sys.stdout.write(str(i)+" ")
+
+
+    ## here we havt to string then only it will print else it will not print it 
+    
