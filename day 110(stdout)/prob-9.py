e5cd354b0833606ddac7778b060fd4ba5a773887
Navya <navyaburrewar2721@gmail.com>
2026-03-30 22:42:48 +0530

    pattern-2

---
+++ day 110(stdout)/prob-9.py
@@ -0,0 +1,14 @@
+# ✅ 4. Print Pattern (Right Triangle)
+
+# Task:
+
+# *
+# **
+# ***
+# ****
+
+import sys
+n=8
+for i in range(n):
+    for j in range(i+1):
+        sys.stdout.write("*" * j+"\n")
