07226af853e12a413eaf468f9a8c6d1acabf49bd
Navya <navyaburrewar2721@gmail.com>
2026-03-31 23:04:54 +0530

    theory

---
+++ day103(sys.stderr)/theory -1.py
@@ -0,0 +1,12 @@
+# #  1 .sys. stderr
+# 👉 Simple Meaning:
+
+# sys.stderr is used to show error messages (like warnings or problems).
+# Normally, print() shows output, but stderr shows errors separately.
+
+# 🧠 Think:
+
+# ➡️ print() → normal message
+# ➡️ sys.stderr → error message
+
+
