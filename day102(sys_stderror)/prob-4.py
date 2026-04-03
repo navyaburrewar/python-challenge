a72c5f5da77257891fdb97db8cb32f80b1575a3e
Navya <navyaburrewar2721@gmail.com>
2026-04-03 21:55:43 +0530

    value

---
+++ day102(sys_stderror)/prob-4.py
@@ -0,0 +1,3 @@
+import sys
+print("this is normal output... ")
+sys.stderr.write("This is a error message\n")
--- day99(sys.path)/prob-3.py
+++ day99(sys.path)/prob-3.py
@@ -4,7 +4,8 @@
 import sys
 print(sys.version)
 
-
-
+## the about on give s the full string   (detailed)
 import sys
 print(sys.version.split()[0])
+
+## only version will give   (clean $ ml)
