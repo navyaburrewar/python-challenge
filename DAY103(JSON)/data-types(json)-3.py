57175d6bd8f2cd2539c4ce7eb94aef6c71bf1368
Navya <navyaburrewar2721@gmail.com>
2026-04-05 23:56:39 +0530

    data types in json

---
+++ DAY103(JSON)/data-types(json)-3.py
@@ -0,0 +1,12 @@
+# Key JSON Data Types
+
+
+
+# JSON supports the following data types:
+
+# String: A sequence of characters, e.g., "hello".
+# Number: Integer or floating-point numbers, e.g., 10, 3.14.
+# Boolean: A value representing true or false.
+# Array: An ordered list of values.
+# Object: A collection of key-value pairs.
+# Null: A null value indicating the absence of any value.
