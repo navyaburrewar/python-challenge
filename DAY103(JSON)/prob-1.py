cae70a766de180f0c44e8d1626697d558d3c024d
Navya <navyaburrewar2721@gmail.com>
2026-04-05 23:53:27 +0530

    basic structure

---
+++ DAY103(JSON)/prob-1.py
@@ -0,0 +1,37 @@
+## basic stucture 
+
+# the basic struture of json consists of two primary components.
+
+
+#  objects : These are enclosed in curly braces {} and contain key-value pairs.
+# Arrays:   Arrays are ordered lists enclosed in square brackets [].
+
+
+
+
+
+# Objects in JSON ##########################3
+# A JSON object is a collection of key-value pairs enclosed in curly braces {}. The key is always a string, and the value can be a variety of data types, including strings, numbers,arrays and even other objects.
+
+# Example:
+
+# { "name": "Mohit Kumar",
+#    "age": 30,
+#    "isStudent": false }
+
+# In this example, name, age, and isStudent are keys, and "John Doe", 30, and false are their respective values.
+
+
+
+
+
+
+# Arrays in JSON#################################
+
+
+
+# A JSON array is an ordered collection of values enclosed in square brackets []. These values can be of any type, including objects, arrays, or primitive data types.
+# Example:
+# { "fruits": ["apple", "banana", "cherry"] }
+
+# Here, fruits is a key, and the value is an array containing the elements "apple", "banana", and "cherry".
