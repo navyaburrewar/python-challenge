2f526bb1384b98ed0f53a1c520d055e8448739d5
Navya <navyaburrewar2721@gmail.com>
2026-04-05 23:13:22 +0530

    theory

---
+++ DAY103(JSON)/theory.py
@@ -0,0 +1,8 @@
+# ## JSON
+# #  JSON (JavaScript Object Notation) is a widely-used, lightweight data format for representing structured data.
+
+# Used Extensively : Used in APIs, configuration files, and data exchange between servers and clients.
+# Text-based: JSON is a simple text format, making it lightweight and easy to transmit.
+# Human-readable: It uses key-value pairs, making the structure easy to understand.
+# Language-independent: While it is derived from JavaScript, JSON is supported by many programming languages including Python, Java, PHP, and more.
+# Supported Data structures: Represents data as objects, arrays, strings, numbers, booleans, and null.
