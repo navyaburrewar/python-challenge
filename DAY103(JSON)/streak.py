e21b3cddbf14b5cb18b9c445ee0e86da78420f1b
Navya <navyaburrewar2721@gmail.com>
2026-04-06 23:37:05 +0530

    probb

---
+++ DAY103(JSON)/streak.py
@@ -0,0 +1,13 @@
+
+## working with json in pyhton
+
+# Python provides a built-in json module to work with JSON data.
+# /We can use the json.loads() method to convert a JSON string to a dictionary 
+#  json.dumps() to convert a dictionary to a JSON string.
+
+
+
+## parse json to dictionary
+
+# In Python, a JSON string can be converted into a dictionary using the built-in json module using json.loads().
+
