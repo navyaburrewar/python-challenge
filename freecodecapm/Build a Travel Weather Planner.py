ac461fef61b7078a5ad9cddbbad7d996e4ab46b1
Navya <navyaburrewar2721@gmail.com>
2026-04-07 11:35:05 +0530

    buid a travel weather palnner

---
+++ freecodecapm/Build a Travel Weather Planner.py
@@ -0,0 +1,24 @@
+distance_mi = 3
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = True
+
+if not distance_mi:
+    print(False)
+
+elif distance_mi <= 1 and is_raining:
+    print(False)
+
+elif distance_mi <= 1 and not is_raining:
+    print(True)
+
+elif 1 < distance_mi <= 6 and has_bike and not is_raining:
+    print(True)
+
+elif distance_mi > 6 and (has_car or has_ride_share_app):
+    print(True)
+
+else:
+    print(False)
+
