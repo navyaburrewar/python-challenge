e552b8d92442ab81144070fa8d9390c862985a2d
Navya <navyaburrewar2721@gmail.com>
2026-04-09 22:48:15 +0530

    json.dumps

---
+++ day104(json-1)/json.dumps-2.py
@@ -0,0 +1,13 @@
+import json
+x={
+    "name": "navya",
+    "age": 20,
+    "city": "hyd"
+}
+
+y=json.dumps(x)
+print(y)
+
+
+##3 the output code will be in the formate of string where we will not understand the values here 
+## it is just a text
