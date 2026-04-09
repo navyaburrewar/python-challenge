ff24afbe46a396b3d9661258370fe00650a1b810
Navya <navyaburrewar2721@gmail.com>
2026-04-09 22:52:08 +0530

    json.dumps

--- day104(json-1)/json.dumps-2.py
+++ day104(json-1)/json.dumps-2.py
@@ -10,4 +10,16 @@ print(y)
 
 
 ##3 the output code will be in the formate of string where we will not understand the values here 
-## it is just a text
+## it is just a text, we wil not access the values
+
+
+import json
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
