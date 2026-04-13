8a47c18e626f3a613ff48a1cb8953ec459bc665d
Navya <navyaburrewar2721@gmail.com>
2026-04-13 23:24:24 +0530

    str-2

---
+++ streak/streak-3.py
@@ -0,0 +1,14 @@
+#   problem-1
+#   # print the one by one values as per the user input --
+# input: 100
+# output: line by line values which are divisible by 3
+
+
+def div(m):
+    for i in range(m) :
+        if i%10==0:
+            yield i
+        
+m=int(input())
+for j in div(m):
+    print(j)
