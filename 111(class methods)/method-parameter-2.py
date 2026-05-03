768ec5a5250a9b6f4356d71e41436aa880c3e8af
Navya <navyaburrewar2721@gmail.com>
2026-05-03 14:16:15 +0530

    method-parameter

--- 111(class methods)/method-parameter-2.py
+++ 111(class methods)/method-parameter-2.py
@@ -1,2 +1,11 @@
 ## method -parameter
 
+class calc:
+    def add(self,a,b):
+        return a+b
+    def multi(self,a,b):
+        return a*b
+    
+cal=calc()      
+print(cal.add(4,8))
+print(cal.multi(5,9))
