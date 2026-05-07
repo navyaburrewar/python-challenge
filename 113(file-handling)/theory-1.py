5cc7c447c783352e77e055657461a176eecd0694
Navya <navyaburrewar2721@gmail.com>
2026-05-07 18:19:55 +0530

    theory

---
+++ 113(file-handling)/theory-1.py
@@ -0,0 +1,28 @@
+# python file-open
+
+# File handling is an important part of any web application.
+# Python has several functions for creating, reading, updating, and deleting files.
+
+
+
+# 1. What is File Handling?
+
+# File handling means:
+
+# Creating files
+# Opening files
+# Reading data
+# Writing data
+# Updating/modifying data
+# Deleting files
+# Managing file paths and folders
+
+# Python lets your programs store data permanently instead of losing everything when the program stops.
+
+# Example:
+
+# Notes app → stores notes in files
+# Login system → stores users
+# Data science → reads CSV/Excel files
+# Web apps → upload/download files
+# Automation → process logs/reports
