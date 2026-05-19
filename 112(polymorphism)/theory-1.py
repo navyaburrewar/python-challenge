4682cc41ccb51ea2637481ff89edf0afe80e30e7
Navya <navyaburrewar2721@gmail.com>
2026-05-19 11:46:29 +0530

    theory-polymarphism

---
+++ 112(polymorphism)/theory-1.py
@@ -0,0 +1,5 @@
+# polymorphism
+#Same method name behaves differently for different objects.
+#Can be method overloading or method overriding.
+# create a parent class animal having sound as a method, child class dog having as a method
+#  -- method overriding having same method but behave differently depend on object
