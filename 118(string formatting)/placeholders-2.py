c5c12ca7a88789a4d4d122b88f75bfdc91e18f6c
Navya <navyaburrewar2721@gmail.com>
2026-05-20 21:57:42 +0530

    placeholders

---
+++ 118(string formatting)/placeholders-2.py
@@ -0,0 +1,13 @@
+#  placeholders and modifiers
+
+#  To format values in an f-string, add placeholders {}, a placeholder can contain variables, operations, functions, and modifiers to format the value.
+
+price =50
+txt =f"the price of apple is {price}"
+print(txt)
+
+
+## ex-2
+quantity=12
+txt = f"we will get {quantity} pens for 100rupees"
+print(txt)
