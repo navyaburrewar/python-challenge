de0fc8f76d259bbfcb354faae2d876e2315407da
Navya <navyaburrewar2721@gmail.com>
2026-05-20 15:51:07 +0530

    exception

---
+++ 116-(pip)/file-1.py
@@ -0,0 +1,15 @@
+# what is pip?
+# PIP is a package manager for Python packages,
+
+#  what is a package?
+# A package contains all the files you need for a module.
+
+# Modules are Python code libraries you can include in your project.
+
+#  using packing
+# example
+
+import camelcase
+c=camelcase.CamelCase()
+txt="hello world"
+print(c.hump(txt))
