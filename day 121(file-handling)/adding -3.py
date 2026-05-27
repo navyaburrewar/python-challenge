c388bba0fd24d450196998b4076f7b797607edaf
Navya <navyaburrewar2721@gmail.com>
2026-05-27 15:29:46 +0530

    adding

---
+++ day 121(file-handling)/adding -3.py
@@ -0,0 +1,9 @@
+#=========================adding new txt==============================
+#we can add new data with existing, if there is no file it will create
+# with open("b1.txt", "a") as f:
+#     f.write(" python developer\n")
+
+
+
+with open("walk","a")as f:
+    f.write(" walking good for health")
