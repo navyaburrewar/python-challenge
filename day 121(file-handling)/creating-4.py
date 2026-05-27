a3f84a775cdd094adaf4bc63ba22a239cf45cd9c
Navya <navyaburrewar2721@gmail.com>
2026-05-27 15:33:04 +0530

    creating a file

---
+++ day 121(file-handling)/creating-4.py
@@ -0,0 +1,10 @@
+#==========================creation=============
+#creates the file if only there is no file with the name mentioned
+
+# file = open("a1.txt", "x")
+# file.write("This file is created using x mode.")
+# file.close()
+
+file=open("hyd","x")
+file.write("this file is created using the x mode:")
+file.close()
