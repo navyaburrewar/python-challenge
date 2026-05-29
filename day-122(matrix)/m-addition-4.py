e364287518fd5a8d947798c6c0d706c052ef68e4
Navya <navyaburrewar2721@gmail.com>
2026-05-29 16:37:01 +0530

    new matrix

--- day-122(matrix)/m-addition-4.py
+++ day-122(matrix)/m-addition-4.py
@@ -6,7 +6,7 @@ col=2
 for i in  range(rows):
     row=[]
     for j in range(col):
-        j=int(input("enter value: "))
+        value=int(input("enter value: "))
         row.append(j)
     mat.append(row)
 print(mat)        
