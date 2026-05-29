ccc6bd9eefe3a162d5a867c2f4ed9d17b8e4a189
Navya <navyaburrewar2721@gmail.com>
2026-05-29 15:44:59 +0530

    matric-1

---
+++ day-122(hacker-rank)/matrix-1.py
@@ -0,0 +1,42 @@
+# =========matrix============#
+
+# #  in general
+# What is a Matrix?
+
+# A matrix is just a table of numbers arranged in rows and columns.
+
+# Example:
+
+
+# Rows → horizontal
+# Columns → vertical
+
+# This matrix has:
+
+# 2 rows
+# 3 columns
+
+
+#========== How to Create a Matrix in Python=================
+
+# In Python, matrices are usually created using:
+
+# Lists
+# NumPy library (most common and powerful)
+
+# We’ll learn both.
+
+
+matric=[[1,2,3],
+        [3,4,5],
+        [2,1,4]]
+
+print(matric)
+row=len(matric)
+col=len(matric[0])  #matrix[0] → first row → [1, 2, 3] length of that row = 3 columns   
+print(row)
+print(col)
+# == explaintion  ===#
+
+#   here row=len(matrix)
+# here col=len(matrix[0])
