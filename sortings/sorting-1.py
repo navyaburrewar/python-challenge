59d9545b99a13b2dfa368fd1879dfa4fc5784152
Navya <navyaburrewar2721@gmail.com>
2026-06-08 18:36:58 +0530

    sorting-2

---
+++ sortings/sorting-1.py
@@ -0,0 +1,46 @@
+#=========== sorting ===========#
+
+# What is Sorting?
+
+# Sorting means arranging data in a specific order.
+
+# Usually:
+
+# Ascending order → smallest to largest
+# Example: [1, 2, 3, 4, 5]
+# Descending order → largest to smallest
+# Example: [5, 4, 3, 2, 1]
+
+# # Sorting is one of the most important concepts in programming because many operations become faster after sorting:
+
+# # Searching
+# # Finding duplicates
+# # Data analysis
+# # Database operations
+# # Ranking systems
+# # Organizing records
+
+
+
+
+# ===============Why Do We Need Sorting?==========#
+
+# Imagine these numbers:
+
+# [8, 2, 5, 1, 9]
+
+# If you want:
+
+# smallest number
+# largest number
+# binary search
+# median
+# duplicates
+
+# …it becomes easier when the data is sorted.
+
+# After sorting:
+
+# [1, 2, 5, 8, 9]
+
+# Now everything is easier to process.
