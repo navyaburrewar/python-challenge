440f844c321ad2548edcd83c8b6abb73e59c9fda
Navya <navyaburrewar2721@gmail.com>
2026-06-08 18:38:28 +0530

    types-sortings

---
+++ sortings/types-sortings-2.py
@@ -0,0 +1,11 @@
+# Types of Sorting Algorithms
+
+# There are many sorting algorithms:
+
+# Algorithm	       Speed	    Easy to Learn
+# Bubble Sort	   Slow	        Very Easy
+# Selection Sort	Slow	      Easy
+# Insertion Sort	Medium	      Easy
+# Merge Sort	    Fast	        Medium
+# Quick Sort	   Very Fast	      Hard
+# Heap Sort	Fast	Hard
