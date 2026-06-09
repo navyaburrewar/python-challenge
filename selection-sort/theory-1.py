282ed54c52799733d2be584c89ae6df20de550dd
Navya <navyaburrewar2721@gmail.com>
2026-06-09 21:30:03 +0530

    theory

---
+++ selection-sort/theory-1.py
@@ -0,0 +1,21 @@
+# Selection Sort — Complete Detailed Explanation
+
+# Selection Sort is one of the simplest sorting algorithms in Computer Science.
+# It works by repeatedly selecting the smallest element from the unsorted part of the array and placing it at the correct position.
+
+
+
+
+
+# ============== 1. Basic Idea of Selection Sort==============#
+
+# Imagine you have cards with numbers:
+
+# 29 10 14 37 13
+
+# You search for the smallest number and place it at the beginning.
+
+# Then again search the smallest from the remaining elements.
+
+# Repeat until the array becomes sorted.
+
