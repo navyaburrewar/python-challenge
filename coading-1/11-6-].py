2cf58f21b7ddfa61ca449960009c916012542c35
Navya <navyaburrewar2721@gmail.com>
2026-06-11 15:57:25 +0530

    findmin

---
+++ coading-1/11-6-].py
@@ -0,0 +1,5 @@
+
+def findMin( nums):
+    return min(nums)  
+    
+print(findMin([1,2,3,4,5,61,0]))    
---
+++ selection-sort/prob-11.py
@@ -0,0 +1,42 @@
+
+
+"""
+Bonus Challenge Problems
+
+If you finish all 10:
+
+Bonus 1
+
+Optimize Selection Sort to avoid unnecessary swaps.
+
+Bonus 2
+
+Implement Selection Sort recursively.
+
+Bonus 3
+
+Visualize Selection Sort step-by-step using prints.
+
+Example:
+
+After Pass 1: [11, 25, 12, 22, 64]
+After Pass 2: [11, 12, 25, 22, 64]
+Recommended Order
+
+Solve in this order:
+
+1 → 2 → 3 → 4 → 5 → 6 → 8 → 7 → 9 → 10
+Important Concepts These Problems Cover
+Problem	Concept
+1	Basic sorting
+2	Descending sort
+3	Swaps
+4	Comparisons
+5	User input
+6	Dry run understanding
+7	String sorting
+8	Manual swapping
+9	Logic application
+10	Custom sorting
+
+"""
