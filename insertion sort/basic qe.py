98329d66c6b1faac8ab8356131799c1427db08e1
Navya <navyaburrewar2721@gmail.com>
2026-06-19 23:02:53 +0530

    basic problem

---
+++ insertion sort/basic qe.py
@@ -0,0 +1,12 @@
+# 1. What is Insertion Sort?
+# Interview-Level Answer
+
+# Insertion Sort is a simple comparison-based sorting algorithm that builds the final sorted array one element at a time. It works by taking each element from the unsorted portion and inserting it into its correct position in the already sorted portion of the array.
+
+# Example:
+
+# Array: [5, 2, 4, 6]
+
+# Pass 1: [2, 5, 4, 6]
+# Pass 2: [2, 4, 5, 6]
+# Pass 3: [2, 4, 5, 6]
