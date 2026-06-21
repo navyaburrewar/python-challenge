9495204bfcc9535c953af6635191196b94a9f65a
Navya <navyaburrewar2721@gmail.com>
2026-06-21 22:50:32 +0530

    prob-3

---
+++ insertion sort/prob-8.py
@@ -0,0 +1,17 @@
+# 10. Binary Insertion Sort
+
+# Use binary search to find the insertion position.
+
+def funct(arr):
+    for i in range(1,len(arr)):
+        key=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>key:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+    return arr
+print(funct([5, 2, 4, 6, 1, 3]))            
+
+
+
