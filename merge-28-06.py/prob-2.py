001fd64dec5cf7bc8a7aad28cdde9059f235f8dd
Navya <navyaburrewar2721@gmail.com>
2026-06-28 22:17:13 +0530

    prob-2

---
+++ merge-28-06.py/prob-2.py
@@ -0,0 +1,33 @@
+#  Sort an array in descending order using Merge Sort
+
+
+def merge_sort(arr):
+    n=len(arr)
+    l=0
+    r=n-1
+    if n>1:
+        mid=(l+r)//2
+        left=arr[:mid+1]
+        right=arr[mid+1:]
+        merge_sort(left)
+        merge_sort(right)
+        i=j=k=0
+        while i<len(left) and j<len(right):
+            if left[i]<right[j]:
+                arr[k]=right[j]
+                j+=1
+            else:
+                arr[k]=left[i]
+                i+=1
+            k+=1
+        while i<len(left):
+            arr[k]=left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            arr[k]=right[j]
+            j+=1
+            k+=1
+        return arr
+
+print(merge_sort([5,4,8,2,1]))        
