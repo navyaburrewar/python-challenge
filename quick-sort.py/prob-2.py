804adcc54a2b7f607cab35398fc9c6f5918f1f8e
Navya <navyaburrewar2721@gmail.com>
2026-06-29 18:51:57 +0530

    one partition step

--- quick-sort.py/prob-2.py
+++ quick-sort.py/prob-2.py
@@ -1,15 +1,17 @@
-
+# Perform one partition step on the array:
 # [8, 3, 1, 7, 0, 10, 2]
 
 # using the last element as the pivot.
 
+
+
 def quick_sort(arr):
-    if len(arr)<=1:
+    n=len(arr)
+    if n<=1:
         return arr
     pivot=arr[-1]
     left=[x for x in arr if x<pivot]
     middle=[x for x in arr if x==pivot]
     right=[x for x in arr if x>pivot]
-    return quick_sort(left)+middle+quick_sort(right)
-
-print("sorted arrray", quick_sort([8, 3, 1, 7, 0, 10, 2]))
+    return quick_sort(left)+middle+right
+print(quick_sort([8, 3, 1, 7, 0, 10, 2]))
