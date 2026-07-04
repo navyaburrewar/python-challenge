4de357b5250dbfa2e299adb6b7673f29900df8ee
Navya <navyaburrewar2721@gmail.com>
2026-07-04 11:14:01 +0530

    prob-13--alphabet

---
+++ 01-07-2026(quick sort)/prob-13.py
@@ -0,0 +1,34 @@
+# 9. Sort Strings
+
+# Input
+
+# ["orange","apple","banana"]
+
+# Expected Output
+
+# ["apple","banana","orange"]
+
+
+def partition(arr,low,high):
+    
+    pivot=arr[high]
+    i=low-1
+    for j in range(low,high):
+        
+        if arr[j]<pivot:
+            i+=1
+            arr[i],arr[j]=arr[j],arr[i]
+            
+        
+    arr[i+1],arr[high]=arr[high],arr[i+1]
+    return i+1
+def quick_sort(arr,low,high):
+    if low<high:
+        pi=partition(arr,low,high)
+
+        quick_sort(arr,low,pi-1)
+        quick_sort(arr,pi+1,high)
+
+arr=["orange","apple","banana"]
+quick_sort(arr,0,len(arr)-1)
+print(arr)   
