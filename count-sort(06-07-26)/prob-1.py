30b796329acfadcf73a624473c022d75e4d66402
Navya <navyaburrewar2721@gmail.com>
2026-07-06 22:30:37 +0530

    prob-1

---
+++ count-sort(06-07-26)/prob-1.py
@@ -0,0 +1,15 @@
+# What is Counting Sort?
+
+# Counting Sort is a non-comparison-based sorting algorithm used to sort integers when the range of input values is not very large.
+
+# Unlike algorithms such as Quick Sort or Merge Sort, Counting Sort does not compare elements. Instead, it counts how many times each element occurs and then uses this information to place elements in their correct positions.
+
+# ///Case	Time Complexity
+# Best Case	O(n + k)
+# Average Case	O(n + k)
+# Worst Case	O(n + k)
+
+# Where:
+
+# n = Number of elements
+# k = Range of input values (max - min + 1)
