e00caf94ac1e87645b3f6e6fd93534755a7d1283
Navya <navyaburrewar2721@gmail.com>
2026-07-21 20:57:32 +0530

    prob-1

--- 20-07-26(sliding window)/prob-1.py
+++ 20-07-26(sliding window)/prob-1.py
@@ -2,4 +2,32 @@
 # SUB Array having more number of vowels
 
 
-arr=["a", "i"," o", "r", "t","u","o"]
+arr=["a", "i","o", "r", "t","u","o"]
+k=4
+vowels=["a","e","i","o","u"]
+count=0
+for i in range(k):
+    if arr[i] in vowels:
+        count+=1
+      
+max_vowels=count
+max_window=arr[:k]
+
+for i in range(k,len(arr)):
+    if arr[i] in vowels:
+        count+=1
+    if arr[i-k] in vowels:
+        count-=1
+
+    if count >max_vowels:
+        max_vowels=count
+        max_window=arr[i-k+1:i+1]
+
+print(max_window)
+print(max_vowels)        
+
+
+
+
+
+
