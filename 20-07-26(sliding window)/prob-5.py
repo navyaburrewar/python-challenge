a1cb0c6a8abb95a0f26d62b30a9c0670d982fa0b
Navya <navyaburrewar2721@gmail.com>
2026-07-25 21:57:37 +0530

    prob-5

---
+++ 20-07-26(sliding window)/prob-5.py
@@ -0,0 +1,15 @@
+# c=0
+# for i in range(0,len(a)-l+1):
+#     if a[i:i+3 ]==b:
+#         c+=1
+# print(c)
+
+# sum of windows
+a=[1,2,3,4,5,6]
+w=2
+
+for i in range(0,len(a)-w+1):
+    sum=0
+    for k in a[i:i+w]:
+        sum+=k
+    print(sum,end=" ")    
