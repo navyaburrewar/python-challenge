cc93c26548989d14d2805d49062e6789a88106a9
Navya <navyaburrewar2721@gmail.com>
2026-07-27 20:34:56 +0530

    prob-7

---
+++ 20-07-26(sliding window)/prob-7.py
@@ -0,0 +1,21 @@
+## leetcod problem
+
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         current_max = nums[0]
+#         current_min = nums[0]
+#         max_product = nums[0]
+
+#         for i in range(1, len(nums)):
+#             num = nums[i]
+
+#             if num < 0:
+#                 current_max, current_min = current_min, current_max
+
+#             current_max = max(num, current_max * num)
+#             current_min = min(num, current_min * num)
+
+#             max_product = max(max_product, current_max)
+
+#         return max_product
+                
