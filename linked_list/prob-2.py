a7de4fcb9864d85d4f99bbd751d55939b027819f
Navya <navyaburrewar2721@gmail.com>
2026-07-29 15:18:36 +0530

    prob-5

---
+++ linked_list/prob-2.py
@@ -0,0 +1,19 @@
+### create multiple nodes
+n1=10
+n2=20
+n3=30
+n4=40
+
+class AB:
+    def __init__(self,num):
+        self.data=num
+        self.add=None
+n1=AB(10)
+n2=AB(20)
+n3=AB(30)
+n4=AB(40)
+
+print(n1.add)
+print(n1.data)
+
+
