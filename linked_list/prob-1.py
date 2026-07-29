baffa4dd5af61fe2e568ca4a653938a4c0ec2474
Navya <navyaburrewar2721@gmail.com>
2026-07-29 15:13:16 +0530

    prob-1

---
+++ linked_list/prob-1.py
@@ -0,0 +1,7 @@
+class Node:
+    def __init__(self,num):
+        self.data=num
+        self.add=None
+n1=Node(10)
+print(n1)
+print(n1.add)
---
+++ linked_list/theory-1.py
@@ -0,0 +1,31 @@
+## linear data structure
+
+## value may be duplicates but not the address
+## avalue will store the address of the next values
+
+# exam
+[10,4]
+# -->here 10 is the value and the 4 is the address of the next value
+#  we use linked link to efficient use the memory
+
+# 4 type
+#  single linked list
+# double  lined list
+# circular linked list
+## double circular linked list
+
+
+
+#  singly linked list
+# [10,n1]-->[30,n2]-->[80,null]
+
+
+
+# doubley  lined list
+# [null,10,n1]-->[n1,30,n2]
+
+
+
+# circular linked list
+# last node connected to the first
+
