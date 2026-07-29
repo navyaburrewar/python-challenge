7d15f22bf26e4cb71543cfb62639714c3aeb04df
Navya <navyaburrewar2721@gmail.com>
2026-07-29 11:40:23 +0530

    prob

---
+++ 29-07-26(queue)/theory-1.py
@@ -0,0 +1,12 @@
+# queue
+# linear data stracture
+# fifo (fisrst i firstout)
+
+# adding element enque
+## deleting ele dequeue
+
+## list vs queue
+# queue     list
+# enque     append
+## deque    que(0)
+
