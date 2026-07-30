9468846720885bc759436a0de734619b26eeb4aa
Navya <navyaburrewar2721@gmail.com>
2026-07-30 11:10:39 +0530

    prob-5

--- linked_list/prob-4.py
+++ linked_list/prob-4.py
@@ -21,3 +21,24 @@ temp=head
 while temp!=None:
     print(temp.data,end="-->")
     temp=temp.add
+
+print("**")
+#  just address included
+class Node:
+    def __init__(self,num):
+        self.data=num
+        self.add=None
+n1=Node(10)
+n2=Node(20)
+n3=Node(30)
+n4=Node(40)
+
+n1.add=n2
+n2.add=n3
+n3.add=n4
+
+head=n1
+temp=head
+while temp!=None:
+    print(temp.data,temp.add,end="-->")
+    temp=temp.add
