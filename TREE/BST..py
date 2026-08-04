5358f26eb6188e69f95d34fa147c4b6682abd5b2
Navya <navyaburrewar2721@gmail.com>
2026-08-04 11:22:45 +0530

    recursion

---
+++ TREE/BST..py
@@ -0,0 +1,39 @@
+# BST (binary search tree)
+
+# no duplicates
+# data stores  in the sorted manner
+# --> if root is none
+    # -->create node adn make it root
+# --> if samll add at left
+# -->greatest add to right
+
+
+"""
+          17
+         /  \
+       15    19
+      /        \
+    11          25
+   /           /  \
+  7          23   29
+"""
+
+
+[19,24,11,13,7,9,8,26,32,64,31,72,16]
+
+# 3  method to print the bse on the  output
+#  3 types of traversal methods
+# 1.inner order
+# 2.pre -order
+# 3.post-order
+
+## basic recursion 
+# 
+def fact(n):
+    if n==1:
+        return 1
+    return n*fact(n-1)
+ 
+
+n=5
+print(fact(n))
